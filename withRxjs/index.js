862e5df5f152a1de1e621573a7d30f2ffa65d37f
Sakai Toma <o_edo@hotmail.com>
2019-09-19 13:59:24 +0200

    Story completed.

--- withRxjs/index.js
+++ withRxjs/index.js
@@ -1,9 +1,27 @@
-import { range } from "rxjs";
-import { map, filter } from "rxjs/operators";
+const { range, from } = require("rxjs");
+const { map, filter, take } = require("rxjs/operators");
 
-range(1, 200)
-  .pipe(
-    filter(x => x % 2 === 1),
-    map(x => x + x)
-  )
-  .subscribe(x => console.log(x));
+const { generateStudents } = require("../lib/studentsFactory");
+const students = require("../lib/mock-students");
+
+const {
+  compose,
+  haspassed,
+  getAverage,
+  withAverage,
+  studentHasPassed
+} = require("../lib/studentsUtils");
+
+const generatedStudents = generateStudents(1000000);
+
+console.time("Time elapsed");
+const hasPassed = from(students.concat(generatedStudents)).pipe(
+  map(withAverage),
+  filter(studentHasPassed),
+  take(1)
+);
+
+hasPassed.subscribe(x => console.log(x));
+console.log("passed ", hasPassed.getValue);
+
+console.timeEnd("Time elapsed");
