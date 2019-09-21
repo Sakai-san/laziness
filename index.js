968aaa3aa027841f31d6c08de41c57840cf2239e
Sakai Toma <o_edo@hotmail.com>
2019-09-21 22:58:44 +0200

    Update iterator.

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-const { generateStudents } = require("./lib/studentFactory");
-const { compose, haspassed, getAverage } = require("./lib/studentUtils");
+const { generateStudents } = require("./lib/studentsFactory");
+const {
+  compose,
+  haspassed,
+  getAverage,
+  withAverage,
+  studentHasPassed
+} = require("./lib/studentsUtils");
 
-const students = generateStudents(1000);
+const students = require("./lib/mock-students");
+const generatedStudents = generateStudents(1000);
 
-const take = 3;
+const take = 2;
 
 const processing = compose(
-  haspassed,
-  getAverage
+  studentHasPassed,
+  withAverage
 );
 
-students[Symbol.iterator()] = function*() {
-  const takeArray = [];
-  while (takeArray.length < take) {
-    takeArray.push(processing(students.shift()));
+const taken = [];
 
-    yield takeArray;
+function* studentStream(index = 0) {
+  if (students.length - 1 === index) {
+    yield null;
+  } else if (take === taken.length) {
+    yield taken;
+  } else {
+    if (processing(students[index])) {
+      taken.push(students[index]);
+    }
+    yield* studentStream(index + 1);
   }
-};
+}
 
-console.log(students.next());
-console.log(students.next());
-console.log(students.next());
+const iterate = studentStream(0);
+
+console.log(iterate.next().value);
