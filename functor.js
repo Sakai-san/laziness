61f4e34906f55fc47d46fb356b011f80d0a8580d
Sakai Toma <o_edo@hotmail.com>
2019-10-04 08:07:18 +0200

    Implement a functor.

---
+++ functor.js
@@ -0,0 +1,24 @@
+const WrapperForValue = function(value) {
+  this.value = value;
+};
+
+WrapperForValue.prototype.map = function(transform) {
+  this.value = transform(this.value);
+  return this;
+};
+
+const wrapperFor4 = new WrapperForValue(4);
+var newFunctor0 = wrapperFor4.map(value => value + 21);
+console.log("newFunctor0 : ", newFunctor0);
+
+var newFunctor1 = newFunctor0.map(value => value + 3).map(value => value + 1);
+
+// le but de la monad est de composer un functor
+
+const combine = x => newFunctor1(newFunctior0(x));
+
+// wrap (lift) take a raw value and wrap it inside a container
+// map apply a function to a wrapped value
+// bind compose wrapped values with each other
+
+// https://www.softdevtube.com/2019/01/08/pragmatic-guide-to-functional-geekery/
