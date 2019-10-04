b3b785287222c057026a69e2cd31494cf0c685e1
Sakai Toma <o_edo@hotmail.com>
2019-10-04 08:23:25 +0200

    Functor must return a new instance but preserve the type.

--- functor.js
+++ functor.js
@@ -3,8 +3,8 @@ const WrapperForValue = function(value) {
 };
 
 WrapperForValue.prototype.map = function(transform) {
-  this.value = transform(this.value);
-  return this;
+  // this.value = transform(this.value);
+  return new WrapperForValue(transform(this.value));
 };
 
 const wrapperFor4 = new WrapperForValue(4);
@@ -12,10 +12,11 @@ var newFunctor0 = wrapperFor4.map(value => value + 21);
 console.log("newFunctor0 : ", newFunctor0);
 
 var newFunctor1 = newFunctor0.map(value => value + 3).map(value => value + 1);
+console.log("newFunctor1 : ", newFunctor1);
 
 // le but de la monad est de composer un functor
 
-const combine = x => newFunctor1(newFunctior0(x));
+// const combine = x => newFunctor1(newFunctior0(x));
 
 // wrap (lift) take a raw value and wrap it inside a container
 // map apply a function to a wrapped value
