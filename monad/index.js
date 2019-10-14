a59b2e2bcc0de7e3855c068ce52c63b2bfa3c7b0
Sakai Toma <o_edo@hotmail.com>
2019-10-14 08:49:10 +0200

    Test with monet.

--- monad/index.js
+++ monad/index.js
@@ -1,5 +1,6 @@
 const students = require("../lib/mock-students");
-const { generateStudents } = require("../lib/studentsFactory");
+const { sumOfGrades } = require("../lib/studentsUtils");
+const { List, Maybe, Just, Either } = require("monet");
 
 const studentsNotWellFormed = [
   { name: "Liam" },
@@ -9,4 +10,15 @@ const studentsNotWellFormed = [
 ];
 
 const moreStudents = [...students, ...studentsNotWellFormed];
-console.log(moreStudents);
+
+const passedStudents = List.fromArray(moreStudents)
+  .map(st => ({
+    ...st,
+    Grades: st.grades ? Maybe.of(st.grades) : Maybe.fromNull()
+  }))
+  .map(st => ({
+    ...st,
+    avg: Either.of(st.Grades)
+  }));
+
+console.log("passedStudents", passedStudents.flatMap(s => s));
