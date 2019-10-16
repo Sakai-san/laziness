1ddae7699b5ee86eeb13367e71512dce4a7e10b9
Sakai Toma <o_edo@hotmail.com>
2019-10-16 07:04:54 +0200

    Attempt to rewrite code with monade.

--- monad/index.js
+++ monad/index.js
@@ -11,7 +11,7 @@ const studentsNotWellFormed = [
 
 const moreStudents = [...students, ...studentsNotWellFormed];
 
-const passedStudent2 = List.fromArray(moreStudents)
+const passedStudent2 = moreStudents
   .map(st => ({
     ...st,
     Grades: st && st.grades ? Maybe.of(st.grades) : Maybe.fromNull([])
@@ -27,7 +27,6 @@ const passedStudent2 = List.fromArray(moreStudents)
         ? st.Sum / st.Grades.fold()(v => v.length)
         : 0
   }))
-  .filter(st => st.Avg >= 4.0)
-  .toArray();
+  .filter(st => st.Avg >= 4.0);
 
 console.log("passed", passedStudent2);
